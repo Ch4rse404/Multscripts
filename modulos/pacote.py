d1d643f3d445654533a1e2bd75946681d4e9e48a
Ch4rse da 404 <107947345+Ch4rse404@users.noreply.github.com>
2022-10-05 14:48:45 -0300

    Create pacote.py

---
+++ modulos/pacote.py
@@ -0,0 +1,23 @@
+import os,time
+
+os.system('clear')
+os.system('figlet pacotes')
+
+def pacote():
+    os.system('apt update')
+    os.system('apt upgrade')
+    os.system('apt install python')
+    os.system('apt install python2')
+    os.system('apt install python3')
+    os.system('apt install pip')
+    os.system('pip install requests')
+    os.system('apt install git')
+    os.system('apt install nano')
+    os.system('apt install neoferch')
+    os.system('neofach')
+pacote()
+
+os.system('figlet adeus')
+print("codded by ch4rse")
+time.sleep(1)
+os.system('exit')
